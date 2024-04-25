7d353c43b8372f83fba2ded5eb9bf4487926354b
panntod <pandhu.munjalindra@gmail.com>
2024-04-25 18:48:17 +0700

    refactor: Create navbar more simply code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,76 +1,52 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-function NavItem({ href, text }) {
-  return (
-    <a
-      href={href}
-      className="text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-      aria-current="page"
-    >
-      {text}
-    </a>
-  );
-}
-
-function MobileMenu() {
-  return (
-    <div className="mobile-hidden sm:hidden" id="mobile-menu">
-      <div className="flex flex-col text-center space-x-1 px-2 pb-3 pt-2">
-        <NavItem href="/" text="Home" />
-        <NavItem href="/todos" text="Todo List" />
-        <NavItem href="/notes" text="Notes" />
-        <NavItem href="/expanse" text="Expanse Tracker" />
-      </div>
-    </div>
-  );
-}
-
+const navOptions = [
+  { title: "Beranda", href: "/" },
+  { title: "Todo List", href: "/todos" },
+  { title: "Note", href: "/notes" },
+  { title: "Expense Tracker", href: "/expenses" },
+];
 
 const Navbar = () => {
-  const removeNavbar = (event) => {
-    event.preventDefault();
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    if (mobileMenu) {
-      mobileMenu.classList.toggle("mobile-hidden");
-    }
-  };
+  const location = useLocation();
 
   return (
-    <>
-      <nav className="bg-gray-800 fixed z-50 w-full">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="text-3xl d-flex ms-5 md:hidden">
-              <button
-                className="navbar-toggler border-0"
-                onClick={removeNavbar}
-                type="button"
-                id="navbarToggle"
-              >
-                <span className="text-white">&#8801;</span>
-              </button>
-            </div>
+    <nav className="bg-gray-800 fixed z-50 w-full">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="text-3xl d-flex ms-5 md:hidden">
+            <button className="navbar-toggler border-0" type="button">
+              <span className="text-white">&#8801;</span>
+            </button>
+          </div>
 
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
-              <div className="flex flex-shrink-0 items-center">
-                <h1 className="font-semibold text-3xl">React Tools</h1>
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  <NavItem href="/" text="Home" />
-                  <NavItem href="/todos" text="Todo List" />
-                  <NavItem href="/notes" text="Notes" />
-                  <NavItem href="/expanse" text="Expanse Tracker" />
-                </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+            <div className="flex flex-shrink-0 items-center">
+              <h1 className="font-semibold text-3xl">React Tools</h1>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navOptions.map((navOption) => (
+                  <Link
+                    to={navOption.href}
+                    className={`text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ${
+                      location.pathname.split("/")[1] ===
+                      navOption.href.split("/")[1]
+                        ? "bg-gray-900"
+                        : ""
+                    }`}
+                    key={navOption.title}
+                  >
+                    {navOption.title}
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
         </div>
-
-        <MobileMenu />
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
