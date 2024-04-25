cf8b485b74b82631a8f3b87939ce95071c5122ba
panntod <pandhu.munjalindra@gmail.com>
2024-04-25 18:46:22 +0700

    refactor: Replacement location navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/home";
 import Todolist from "./pages/todolist";
 import NotFound from "./pages/notFound";
 import Note from "./pages/note";
 import Expense from "./pages/expense";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const App = () => {
-
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="*" element={ <NotFound/> } />
-        <Route path="/" element={ <Home/> }/>
-        <Route path="/todos" element={ <Todolist/> }/>
-        <Route path="/notes" element={ <Note/> }/>
-        <Route path="/expanse" element={ <Expense/> }/>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<Todolist />} />
+        <Route path="/notes" element={<Note />} />
+        <Route path="/expenses" element={<Expense />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
